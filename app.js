43aee94e9ea4faf053103de1e82c196b9782290c
efrenpar <efrenbarcevr@gamil.com>
2021-01-17 00:26:39 -0500

    inicial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require("path");
+var router = express.Router();
+const http = require("http");
+var server = require('http').Server(app);
+const host = 'localhost';
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+
+app.use('/css',express.static('css'));
+app.use('/fonts',express.static('fonts'));
+app.use('/img',express.static('img'));
+app.use('/js',express.static('js'));
+app.use('/pdf',express.static('pdf'));
+app.use('/',router);
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+  console.log("GET index.html");
+});
+
+
+
+app.use(function(req, res, next) {
+    res.status(404).send("page not found");
+    console.error(res.statusCode+"page not found");
+  
+  });
+
+  
+server.listen(port, host, function() {
+  console.log("Server started.......");
+});
---
+++ js/porcentual.js
@@ -0,0 +1,186 @@
+const api_url = 'http://localhost:5000/api/3/action'
+const accionBuscarRecursos = '/current_package_list_with_resources'
+const accionSQL = '/datastore_search_sql'
+const accionResourchSearch = "/resource_search"
+const organization = 'sistema-seguimiento'
+const token = 'c35c1d70-45c8-40d7-a19f-c8cc0a4fa53a'
+const search = '/datastore_search'
+const datastoreSQL = '/datastore_search_sql'
+
+function createJoinSQL(prestacion,dinardap){
+    var query = "select count(*)/(SELECT COUNT(*) " 
+    +"FROM \""+prestacion+"\" )*100 as porcentaje "
+    +",count(t1._id) as ninos, count(*) as prestacion " 
+    +"from \""+prestacion+"\" as t1 " 
+    +"inner join \""+dinardap+"\" as t2 "
+    +"on t1.\"DOCUMENTOIDENTIDAD\"=t2.\"DOCUMENTOIDENTIDAD\""
+    return query;
+}
+
+function errorRequest(err){
+    switch(err.status){
+        case "400":
+            console.log("bad request")
+            break
+        case "401":
+            console.log("unauthorized");
+            break;
+        case "403":
+            console.log("forbidden");
+            break;
+        default:
+            console.log("unknown error");
+            break;
+    }
+}
+
+function getPorcentajes(query){
+    return new Promise((resolve,reject)=>{
+        console.log(query);
+        $.ajax({
+            url:api_url+datastoreSQL,
+            type:'POST',
+            data:{
+                sql:query 
+            },
+            headers: {
+                authorization: token
+            },
+            dataType:'json',
+            error:function(err){
+                errorRequest(err)
+            },
+            success:function({result}){
+                resolve(result.records[0])
+            }
+
+        });
+    })
+}
+
+function getProvincias(resourcesDict){
+    $.ajax({
+        url:api_url+search,
+        type:'POST',
+        data:{
+            resource_id: resourcesDict['ninos_dinardap'],
+	        fields:"PROVINCIA_CENTRO",
+        },
+        headers: {
+            authorization: token
+        },
+        dataType:'json',
+        error:function(err){
+            errorRequest(err)
+        },
+        success:function(result){
+            console.log(result)
+        }
+
+    });
+
+
+}
+
+function boxTemplate(name,recurso){
+    $("#porcentajes").append(' <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">'
+    +'<div class="analytics-sparkle-line reso-mg-b-30">'
+            +'<div class="analytics-content">'
+            +'<h5>'+name+'</h5>'
+            +'<h2>#<span class="counter">'+recurso.prestacion+'</span> <span class="tuition-fees">Tuition Fees</span></h2>'
+            +'<span class="text-success">'+recurso.porcentaje+"%</span>"
+            +'<div class="progress m-b-0">'
+                +'<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="70"'
+                    +'aria-valuemin="0" aria-valuemax="100" style="width:'+recurso.porcentaje+'%">'
+                    +'<span class="sr-only">70% Complete</span>'
+                +'</div>'              
+            +'</div>'
+        +'</div>'
+    +'</div>'
++'</div>')
+
+}
+
+function getResourceId(recurso){
+    return new Promise((resolve,reject)=>{
+        var name = recurso;
+        var result={};
+        $.ajax({
+            url:api_url+accionResourchSearch,
+            type:'POST',
+            data:{query: "name:"+name},
+            headers: {
+                authorization: token
+            },
+            dataType:'json',
+            error:function(err){
+                errorRequest(err)
+            },
+            success:function({result}){
+                //var id = result.results[0].id;
+                var id = result.results[0].id
+                result = {
+                    "name":name,
+                    "id":id
+                }
+                resolve(result);
+            }
+    
+        });
+    })
+
+}
+
+function createBoxes({result}){
+    idList= []
+    resourcesDict={}
+    var organizationName = result[0].organization.name;
+    promesaList = [];
+    //console.log(result.result[0].organization.name);
+    var lengthRecursos = result[0].resources.length;
+    var recurso = result[0].resources;
+    if(organization == organizationName){
+        recurso.forEach(element => {
+            promesaList.push(getResourceId(element.name))
+            /*getResourceId(element.name).then((id)=> {
+                resourcesDict[element.name] = id;
+                 if(element.name!="ninos_dinardap"){
+                    var query;
+                    query=createJoinSQL(resourcesDict[element.name],resourcesDict["ninos_dinardap"]);
+                    getPorcentajes(query).then((result)=>{boxTemplate(element.name,result)})
+                     
+                }
+            })*/
+            //boxTemplate(recurso[i]);
+        });
+        if(promesaList.length>0){
+            Promise.all(promesaList).then((value)=>{
+                value.forEach(element=>)
+            })
+        }
+    }
+    
+    
+}
+
+
+function getResources(){
+    return new Promise((resolve,reject)=>{
+        var recursos;
+        $.ajax({
+            url:api_url+accionBuscarRecursos,
+            type:'GET',
+            dataType:'json',
+            error:function(err){
+                errorRequest(err)
+            },
+            success:function(result){
+                resolve(result);
+            }
+    
+        });
+    })
+
+     
+}
+getResources().then((result)=>{createBoxes(result);})
